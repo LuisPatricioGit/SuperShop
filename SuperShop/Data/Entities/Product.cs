54bca548af63ed0cd80d3972421100257480eac3
Luis Patrício <mrsiul.gaming@outlook.pt>
2023-05-29 15:14:47 +0100

    Generic Repository

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -3,8 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SuperShop.Data.Entities
 {
-    public class Product
+    public class Product : IEntity
     {
+
         public int Id { get; set; }
 
         [Required]
