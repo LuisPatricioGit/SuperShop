d2c0efa917a1564800d1cc19b5cc3ca7608d16a3
LuisPatricioGit <luispatricio.info@gmail.com>
2023-06-20 15:17:31 +0100

    Register New Users

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -73,8 +73,7 @@ namespace SuperShop.Controllers
 
                 var product = _converterHelper.ToProduct(model, imageId, true);
 
-                //TODO: Modificar para o user que tiver logado
-                product.User = await _userHelper.GetUserByEmailAsync("luispatricio.info@gmail.com");
+                product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 await _productrepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -121,7 +120,7 @@ namespace SuperShop.Controllers
 
                     var product = _converterHelper.ToProduct(model, imageId, false);
 
-                    product.User = await _userHelper.GetUserByEmailAsync("luispatricio.info@gmail.com");
+                    product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await _productrepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
@@ -141,6 +140,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
