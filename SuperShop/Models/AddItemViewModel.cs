8dfa48df995cf864ceeb30f7cd32c7a6912df09f
LuisPatricioGit <luispatricio.info@gmail.com>
2023-07-03 14:30:09 +0100

    Products combo

---
+++ SuperShop/Models/AddItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SuperShop.Models
+{
+    public class AddItemViewModel
+    {
+        [Display(Name = "Product")]
+        [Range(1, int.MaxValue, ErrorMessage = "You Must Select a Product.")]
+        public int ProductId { get; set; }
+
+
+        [Range(0.0001, int.MaxValue, ErrorMessage = "The Quantity must be a positive number.")]
+        public int Quantity { get; set; }
+
+        public IEnumerable<SelectListItem> Products { get; set; }
+    }
+}
