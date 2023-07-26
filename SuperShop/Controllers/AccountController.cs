33f2b6d931b7d5c632634d65cae1e9e952df3b19
LuisPatricioGit <luispatricio.info@gmail.com>
2023-07-26 16:37:55 +0100

    Cascade Drop Down List

--- SuperShop/Controllers/AccountController.cs
+++ SuperShop/Controllers/AccountController.cs
@@ -2,177 +2,225 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
 using SuperShop.Data.Entities;
 using SuperShop.Helpers;
 using SuperShop.Models;
 
 namespace SuperShop.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly IUserHelper _userHelper;
+    public class AccountController : Controller
+    {
+        private readonly IUserHelper _userHelper;
+        private readonly ICountryRepository _countryRepository;
 
-		public AccountController(IUserHelper userHelper)
-		{
-			_userHelper = userHelper;
-		}
+        public AccountController(IUserHelper userHelper, ICountryRepository countryRepository)
+        {
+            _userHelper = userHelper;
+            _countryRepository = countryRepository;
+        }
 
-		public IActionResult Login()
-		{
-			if (User.Identity.IsAuthenticated)
-			{
-				return RedirectToAction("Index", "Home");
-			}
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
-			return View();
-		}
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var result = await _userHelper.LoginAsync(model);
-				if (result.Succeeded)
-				{
-					if (this.Request.Query.Keys.Contains("ReturnUrl"))
-					{
-						return Redirect(this.Request.Query["ReturnUrl"].First());
-					}
-					return this.RedirectToAction("Index", "Home");
-				}
-			}
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userHelper.LoginAsync(model);
+                if (result.Succeeded)
+                {
+                    if (this.Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(this.Request.Query["ReturnUrl"].First());
+                    }
+                    return this.RedirectToAction("Index", "Home");
+                }
+            }
 
-			this.ModelState.AddModelError(string.Empty, "Failed to Login");
-			return View(model);
-		}
+            this.ModelState.AddModelError(string.Empty, "Failed to Login");
+            return View(model);
+        }
 
-		public async Task<IActionResult> Logout()
-		{
-			await _userHelper.LogoutAsync();
-			return RedirectToAction("Index", "Home");
-		}
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
-		public IActionResult Register()
-		{
-			return View();
-		}
+        public IActionResult Register()
+        {
+            var model = new RegisterNewUserViewModel
+            {
+                Countries = _countryRepository.GetComboCountries(),
+                Cities = _countryRepository.GetComboCities(0)
+            };
 
-		[HttpPost]
-		public async Task<IActionResult> Register(RegisterNewUserViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userHelper.GetUserByEmailAsync(model.Username);
-				if (user == null)
-				{
-					user = new User
-					{
-						FirstName = model.FirstName,
-						LastName = model.LastName,
-						Email = model.Username,
-						UserName = model.Username
-					};
+            return View(model);
+        }
 
-					var result = await _userHelper.AddUserAsync(user, model.Password);
-					if (result != IdentityResult.Success)
-					{
-						ModelState.AddModelError(string.Empty, "The user couldn't be created.");
-						return View(model);
-					}
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterNewUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(model.Username);
+                if (user == null)
+                {
+                    var city = await _countryRepository.GetCityAsync(model.CityId);
 
-					var loginViewModel = new LoginViewModel
-					{
-						Password = model.Password,
-						RememberMe = false,
-						Username = model.Username
-					};
+                    user = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Username,
+                        UserName = model.Username,
+                        Address = model.Address,
+                        PhoneNumber = model.PhoneNumber,
+                        CityId = model.CityId,
+                        City = city
+                    };
 
-					var result2 = await _userHelper.LoginAsync(loginViewModel);
+                    var result = await _userHelper.AddUserAsync(user, model.Password);
+                    if (result != IdentityResult.Success)
+                    {
+                        ModelState.AddModelError(string.Empty, "The user couldn't be created.");
+                        return View(model);
+                    }
 
-					if (result2.Succeeded)
-					{
-						return RedirectToAction("Index", "Home");
-					}
+                    var loginViewModel = new LoginViewModel
+                    {
+                        Password = model.Password,
+                        RememberMe = false,
+                        Username = model.Username
+                    };
 
-					ModelState.AddModelError(string.Empty, "The user couldn't be logged.");
-				}
-			}
-			return View(model);
-		}
+                    var result2 = await _userHelper.LoginAsync(loginViewModel);
 
-		public async Task<IActionResult> ChangeUser()
-		{
-			var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
-			var model = new ChangeUserViewModel();
-			if (user != null)
-			{
-				model.FirstName = user.FirstName;
-				model.LastName = user.LastName;
-			}
+                    if (result2.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
 
-			return View(model);
-		}
+                    ModelState.AddModelError(string.Empty, "The user couldn't be logged.");
+                }
+            }
+            return View(model);
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> ChangeUser(ChangeUserViewModel model)
-		{
-			if (!ModelState.IsValid)
-			{
-				var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
-				if (user != null)
-				{
-					user.FirstName = model.FirstName;
-					user.LastName = model.LastName;
-					var response = await _userHelper.UpdateUserAsync(user);
-					if (response.Succeeded)
-					{
-						ViewBag.UserMessage = "User Updated!";
-					}
-					else
-					{
-						ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);
-					}
-				}
-			}
-			return View(model);
-		}
+        public async Task<IActionResult> ChangeUser()
+        {
+            var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+            var model = new ChangeUserViewModel();
+            if (user != null)
+            {
+                model.FirstName = user.FirstName;
+                model.LastName = user.LastName;
+                model.Address = user.Address;
+                model.PhoneNumber = user.PhoneNumber;
 
-		public IActionResult ChangePassword()
-		{
-			return View();
-		}
+                var city = await _countryRepository.GetCityAsync(user.CityId);
+                if (city != null)
+                {
+                    var country = await _countryRepository.GetCountryAsync(city);
+                    if (country != null)
+                    {
+                        model.CountryId = country.Id;
+                        model.Cities = _countryRepository.GetComboCities(country.Id);
+                        model.Countries = _countryRepository.GetComboCountries();
+                        model.CityId = user.CityId;
+                    }
+                }
+            }
+            model.Cities = _countryRepository.GetComboCities(model.CountryId);
+            model.Countries = _countryRepository.GetComboCountries();
 
-		[HttpPost]
-		public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
-				if (user != null)
-				{
-					var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
-					if (result.Succeeded)
-					{
-						return RedirectToAction("ChangeUser");
-					}
-					else
-					{
-						ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
-					}
-				}
-				else
-				{
-					ModelState.AddModelError(string.Empty, "User not found.");
-				}
-			}
-			return View(model);
-		}
 
-		public IActionResult NotAuthorized()
-		{
-			return View();
-		}
-	}
+            return View(model);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> ChangeUser(ChangeUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    var city = await _countryRepository.GetCityAsync(model.CityId);
+
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.Address = model.Address;
+                    user.PhoneNumber = model.PhoneNumber;
+                    user.CityId = model.CityId;
+                    user.City = city;
+
+                    var response = await _userHelper.UpdateUserAsync(user);
+                    if (response.Succeeded)
+                    {
+                        ViewBag.UserMessage = "User Updated!";
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User not found.");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult NotAuthorized()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Account/GetCitiesAsync")]
+        public async Task<JsonResult> GetCitiesAsync(int countryId)
+        {
+            var country = await _countryRepository.GetCountriesWithCitiesAsync(countryId);
+            return Json(country.Cities.OrderBy(c => c.Name));
+        }
+
+    }
 }
