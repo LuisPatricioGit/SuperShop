83ea74db4f13b86e1853db92d9663ae1935e4667
LuisPatricioGit <LuisPatricio.info@gmail.com>
2023-09-08 12:05:26 +0100

    Token Generate Migration Fix

---
+++ SuperShop/Migrations/20230908105954_TokenGeneration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperShop.Migrations
+{
+    public partial class TokenGeneration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Country_CountryId",
+                table: "Cities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Country",
+                table: "Country");
+
+            migrationBuilder.RenameTable(
+                name: "Country",
+                newName: "Countries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries");
+
+            migrationBuilder.RenameTable(
+                name: "Countries",
+                newName: "Country");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Country",
+                table: "Country",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Country_CountryId",
+                table: "Cities",
+                column: "CountryId",
+                principalTable: "Country",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
